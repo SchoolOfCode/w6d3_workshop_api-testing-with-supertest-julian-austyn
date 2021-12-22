bdaba624ff2f77cff098596fd2de76dba37fabde
Jul-Me <memai.julian@gmail.com>
2021-12-22 11:57:22 +0000

    wokring on task 1

--- task-1/index.test.js
+++ task-1/index.test.js
@@ -1,11 +1,13 @@
 /**
- * Before we start testing APIs, let's get some practice with Jest matchers. Matchers allow us to check if a value meets our expectations.
+ * Before we start testing APIs, let's get some practice with Jest matchers.
+ * Matchers allow us to check if a value meets our expectations.
  *
  * Where necessary, have a look at the Jest docs: https://jestjs.io/docs/expect
  */
 
 /**
- * Write a test that checks whether the variable `apiResponse` contains an object that has the following structure:
+ * Write a test that checks whether the variable `apiResponse`
+ * contains an object that has the following structure:
  *
  *    {
  *      success: true
@@ -18,7 +20,8 @@ test("Has the structure { success: true }", function () {
 });
 
 /**
- * Write a test that checks whether the variable `apiResponse` contains an object that has the following structure:
+ * Write a test that checks whether the variable `apiResponse` contains an object that 
+ * has the following structure:
  *
  *    {
  *      copiesSold: any number,
@@ -29,13 +32,14 @@ test("Has the structure { success: true }", function () {
  */
 test("Has the structure { copiesSold: any number, title: any string }", function () {
   const apiResponse = {
-    copiesSold: 5014,
-    title: "THE LIGHTHOUSE (1984)",
+    copiesSold: expect.any(Number),
+    title: expect.any(String)
   };
 });
 
 /**
- * Write a test that checks whether the asynchronous function `getAuthentication`, when called, resolves to an object that has the following structure:
+ * Write a test that checks whether the asynchronous function `getAuthentication`,
+ * when called, resolves to an object that has the following structure:
  *
  *    {
  *      success: true,
@@ -46,8 +50,10 @@ test("Has the structure { copiesSold: any number, title: any string }", function
  *      }
  *    }
  *
- * Since `getAuthentication` is asynchronous, you may need to read up on how to write an asynchronous test in Jest: https://jestjs.io/docs/asynchronous
+ * Since `getAuthentication` is asynchronous, you may need to read up on how to write an 
+ * asynchronous test in Jest: https://jestjs.io/docs/asynchronous
  */
+
 test("Has the structure { success: true, payload: { hasAuthenticated: true, isAdmin: false, userId: any number } }", function () {
   async function getAuthentication() {
     return {
@@ -61,8 +67,19 @@ test("Has the structure { success: true, payload: { hasAuthenticated: true, isAd
   }
 });
 
+const actual = await getAuthentication();
+const expected = {
+        success: true,
+        payload: {
+          hasAuthenticated: true,
+          isAdmin: false,
+          userId: expect.any(Number)
+        }
+      }
+
 /**
- * Write a test that checks whether the asynchronous function `getUsernames`, when called, resolves to an object that has the following structure:
+ * Write a test that checks whether the asynchronous function `getUsernames`, 
+ * when called, resolves to an object that has the following structure:
  *
  *    {
  *      success: true,
@@ -71,8 +88,14 @@ test("Has the structure { success: true, payload: { hasAuthenticated: true, isAd
  *
  * Only the function has been provided. You'll have to write everything else.
  *
- * Since `getUsernames` is asynchronous, you may need to read up on how to write an asynchronous test in Jest: https://jestjs.io/docs/asynchronous
+ * Since `getUsernames` is asynchronous, you may need to read up on 
+ * how to write an asynchronous test in Jest: https://jestjs.io/docs/asynchronous
  */
+describe('GET /user', function(){
+  it('responds')
+
+})
+
 async function getUsernames() {
   return {
     success: true,
@@ -84,3 +107,10 @@ async function getUsernames() {
     ],
   };
 }
+
+const actual = await getUsernames();
+
+const expected = {
+      success: true,
+      payload: an array of objects with the structure { username: any string },
+    }
